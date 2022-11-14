0c4515674c5355bbd02824e3f17b4a242aba15cd
Tasreen Mowreen <tmowreen97@gmail.com>
2022-11-14 00:10:54 -0500

    commit tree ec91c8a21dbfb123817547a6e907d6de2ac90575

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize (name:, breed:, id: nil)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL 
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+
+
+
+
 
 end
